4261539755b3c994b476ff313da46c9cd46531bd
assballs <ass@balls>
2024-06-14 08:15:54 -0600

    updating config

--- lua/twright/plugins.lua
+++ lua/twright/plugins.lua
@@ -13,12 +13,31 @@ return {{
         vim.keymap.set("i", "<C-k><C-b>", vim.cmd.NERDTreeToggle)
     end,
 },{
-    'akinsho/toggleterm.nvim', 
-    version = "*", 
-    config = true,
-    config = function() 
-        require('toggleterm').setup()
-    end
+    "akinsho/toggleterm.nvim",
+    config = function()
+        require("toggleterm").setup {
+            size = 20,
+            open_mapping = [[<c-\>]],
+            hide_numbers = true,
+            shade_filetypes = {},
+            shade_terminals = true,
+            shading_factor = '1',
+            start_in_insert = true,
+            insert_mappings = true,
+            persist_size = true,
+            direction = 'float',
+            close_on_exit = true,
+            shell = vim.o.shell,
+            float_opts = {
+                border = 'curved',
+                winblend = 0,
+                highlights = {
+                    border = "Normal",
+                    background = "Normal",
+                }
+            }
+        }
+    end,
 },{
     'vimwiki/vimwiki', 
     config = function()
@@ -29,6 +48,8 @@ return {{
             },
         }
     end
+},{
+    'BurntSushi/ripgrep',
 },{
     'nvim-telescope/telescope.nvim',
     dependencies = { 
