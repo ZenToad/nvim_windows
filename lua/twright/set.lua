5ed8e1a5e276656ab8799be1eef435667c6a2c8b
assballs <ass@balls>
2024-06-19 17:17:15 -0600

    updating stuff

--- lua/twright/set.lua
+++ lua/twright/set.lua
@@ -1,33 +1,50 @@
+-- Cursor settings
 vim.opt.guicursor = ""
+
+-- Disable netrw (default file explorer)
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+-- Line numbers
 vim.opt.nu = true
--- vim.opt.relativenumber = true
+-- vim.opt.relativenumber = true  -- Uncomment this if you want relative line numbers
 
+-- Tabs and indentation
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
+-- No line wrapping
 vim.opt.wrap = false
 
+-- Swap and backup files
 vim.opt.swapfile = false
 vim.opt.backup = false
-print(vim.env.HOME)
-vim.opt.undodir = vim.env.HOME.."/.vim/undodir"
+
+-- Undo settings
+-- Ensure the undodir exists
+local undodir = vim.env.HOME .. "/.vim/undodir"
+if not vim.loop.fs_stat(undodir) then
+    vim.loop.fs_mkdir(undodir, 511) -- 511 decimal is 0777 in octal
+end
+vim.opt.undodir = undodir
 vim.opt.undofile = true
 
+-- Search settings
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
+-- Colors
 vim.opt.termguicolors = true
 
+-- Scrolling and sign column
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
 
+-- Update time
 vim.opt.updatetime = 50
 
+-- File name settings
+vim.opt.isfname:append("@-@")
